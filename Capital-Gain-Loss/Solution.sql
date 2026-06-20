abf5725d0bcfffc785ea297c5f642c513ace7987
Mohd Sameer <mohdsameer18n04@gmail.com>
2026-06-20 22:29:31 +0530

    Add SQL query to calculate capital gain/loss by stock

---
+++ Capital-Gain-Loss/Solution.sql
@@ -0,0 +1,9 @@
+SELECT stock_name,
+    SUM(
+        CASE
+            WHEN operation='Sell' THEN price
+            ELSE -price
+        END
+    ) AS capital_gain_loss
+FROM stocks
+GROUP BY stock_name;
